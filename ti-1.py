1888b55e37f1360526ddd33bd25ad4520bdb981b
marthamora <marthamora1235@gmail.com>
2017-06-30 11:55:19 -0500

    Add files via upload

---
+++ ti-1.py
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Off Canvas Template for Bootstrap</title>
+        <!-- Bootstrap core CSS -->
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="offcanvas.css" rel="stylesheet">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    </head>
+    <body>
+        <nav class="navbar navbar-fixed-top navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"></a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active">
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#about"></a>
+                        </li>
+                        <li>
+                            <a href="#contact"></a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.nav-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+        <!-- /.navbar -->
+        <div class="container">
+            <div class="row row-offcanvas row-offcanvas-right">
+                <div class="col-xs-10 col-sm-10">
+                    <p class="pull-right visible-xs"><button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button></p>
+                    <div class="jumbotron">
+                        <h1>Como generan dinero WhatsApp, Google y Snapchat...</h1>
+                        <p>Muchas de las aplicaciones, conocidas como redes sociales, navegadores de internet, te prestan sus servicios sin ningún costo por descarga, si es el caso o bien, de suscripcion, simplemente es gratuito para el usuario, sin embargo de una u otra forma deben generar ingresos para su empresa.</p>
+                    </div>
+                    <div class="row">
+                      <div class="col-xs-5 col-md-5">
+   <a href="#" class="thumbnail">
+       <img src="img/whats.jpg" alt="">
+   </a>
+</div>
+                        <div class="col-xs-18 col-lg-20">
+                            <h2>WhatsApp</h2>
+                            <p>Así gana dinero Whatsapp Escrito por David Trujillo el 31/01/2012 a las 18:53
+Whatsapp es posiblmente la aplicación más conocida entre los smartphones, usada por miles de personas y con una fama enorme. Según el sistema operativo que se use es gratis o cuesta muy poco, así que aunque penséis que Whatsapp no gana dinero al ofrecer su servicio gratuito estáis equivocados, y os explicamos cómo lo hacen.Este mensaje, que empezó entre los usuarios de habla inglesa, se ha repartido como la pólvora, pero es completamente falso. Whatsapp ya confirmóque era un “bulo” y aconseja hacer caso omiso de él.
+
+Pero si, es cierto que después de un año de uso el programa pasa ser de pago para aquellos que deseen seguir utilizando. A excepción de los usuarios de iPhone, que pagan de inicio 0,79 euros por la aplicación. Pero con cada actualización que  recibe la aplicación, el contador vuelve a cero. Y todo es porque a Whatsapp le interesa que que cada vez más y más gente utilice su servicio. Que haya más gente que se “enganche”. El problema es que si lo hacen de pago, el numero de usuarios disminuiría.
+
+Whatsapp ha conseguido algo que muchas empresas quieren, y es que sus usuarios sean fans de su servicio, ha creado una necesidad, algo que se utiliza día a día.Pero, ¿de dónde saca el dinero si no cobran nada? La respuesta se halla en los fondos de inversión, compañías que con el dinero de sus clientes lo invierten en otras empresas en auge. Cuando esas empresas son vendidas o generan beneficios, los inversores se llevan la parte correspondiente a lo que hayan invertido. Whatsapp gana más dinero cuantos más inversores tenga, y los inversores ganan dinero cuanto más famoso sea el servicio, en este caso, cuantos más usuarios tenga Whatsapp. Aunque esto no deja de ser una inversión.
+
+Otra ventaja que tiene Whatsapp es que sus gastos son pocos, ya que además de los servidores hay que pagar a muy pocos trabajadores.
+
+Los fondos de  inversión quieren a WhatsApp por que se ha convertido en una necesidad para la gente. Cualquier empresa querría un servicio así. Imaginad cuánto dinero podrían conseguir si insertaran, por ejemplo, publicidad. Pero tampoco les interesa, al menos no por el momento.
+
+El precio de la compañía es desorbitado en comparación con lo que ha costado montar y mantener WhatsApp. Entonces ya esta todo claro, Whatsapp es un negocio con pocos gastos, millones de usuarios y un gran numero de inversores que desean financiarla. ¡Una autentica joya!
+</p>
+
+                        </div>
+
+
+                        <div class="row">
+                          <div class="col-xs-5 col-md-4">
+                        <a href="#" class="thumbnail">
+                        <img src="img/google.jpg" alt="">
+                        </a>
+                        </div>
+
+
+                        <div class="col-xs-18 col-lg-20">
+                            <h2>Google</h2>
+                            <p> Nada más y nada menos que el Presidente de la compañía, planteó esta pregunta, en una reciente carta a sus lectores, esperemos que retóricamente, ya que seguramente conozca al dedillo las finanzas de su empresa. En el informe anual declaraba que la principal fuente de ingresos de Google es la publicidad online efectiva, siendo ésta un 96% de sus ingresos.
+
+Cuando crearon la compañía allá por 1998, otros buscadores como Ask parecía que iban a ser los buscadores punteros, pero solo dos años después Google era ya un referente padre, comenzando su vertiginoso camino, hasta la que hoy en día es una de las empresas más grandes del mundo. ¿Cómo llegó hasta ese punto?Google Adwords. Cuando google ya era conocida como un motor de búsqueda, en el año 2000, la empresa se dio cuenta que si la gente entraba en un buscador escribiendo unas palabras concretas, poca gente iba a pasar a la segunda página. Por lo que muchas empresas estarían dispuestas a pagar por tener un sitio en esa privilegiada primera página.
+
+Imaginemos que tienes una panadería en Toledo, por ejemplo. Seguro que pagarías por un sitio que visitaran solo personas que estén buscando las palabras “panadería” + “Toledo”, es decir, gente que está buscando una panadería en Toledo.
+
+Google Adwords suele operar por coste por clic, es decir, cada vez que alguien pinche en tu enlace les tienes que pagar una cantidad prefijada. Si nadie hace click en tu página, no les pagas nada, caso que muy raramente se da en la realidad.
+
+Para los accionistas de Google este método es todavía mejor, debido a que sale más arriba quién está dispuesto a pagar más, a través de subastas. Tal vez, panadería en Toledo no sea una de las más palabras más saturadas, pero imagine lo que está dispuesto a pagar la gente por salir en primera línea de palabras como “Hotel en Madrid”, “apuestas online” y todo este tipo de negocios.
+
+Pero, aún hay más. Otra de las gallinas de los huevos de oro se llama Google Adsense. Esta plataforma actúa como intermediario entre los anunciantes y los propietarios de páginas web, permitiendo a los propietarios de páginas web unirse a la red de Google y publicar anuncios de los otros, donde los alogaritmos de google hacen todo el trabajo, insertando la publicidad que interesa a la persona que está navegando por la red.
+
+Según el último informe financiero de Google, sus ingresos por publicidad provienen en un 70% de Adwords y el resto de Adsense.
+
+El resto de aplicaciones de Google, como Google maps, gmail, docs, google +, google earth y demás, aparte de ser servicios muy útiles para la gente, no pretenden nada más que asentar una base sólida de la compañía, para que la gente pase más tiempo en Google y así ellos puedan conocer mejor nuestros gustos, nuestros intereses, con quién te relacionas y hasta dónde has estado. Todo ello para que hagas más clicks en los anuncios y así ganar dinero.
+
+
+</p>
+
+                        </div>
+
+
+                        <div class="row">
+                          <div class="col-xs-5 col-md-4">
+                        <a href="#" class="thumbnail">
+                        <img src="img/snap.jpg" alt="">
+                        </a>
+                        </div>
+
+                        <div class="col-xs-18 col-lg-20">
+                            <h2>Snapchat</h2>
+                            <p>Las 3 formas principales de monetización de Snapchat están enfocadas en la cámara, el contenido y la comunicación.
+Cámara
+En términos de monetización  de la cámara, Snapchat ha desarrollado un negocio con los “geofiltros” para las marcas. Esto le permite a las empresas y a gente particular crear filtros dentro de las famosas máscaras. Por ejemplo, con la presentación de la película de X-Men: Apocalypse se lanzó un filtro con personajes de la película. Además se icluyó una funcionalidad para poder comprar las entradas directamente, lo que añade una forma más de obtener ingresos. También se pueden conseguir filtros que cubran una cierta zona, de manera que toda la gente pueda utilizar ese filtro al estar en el sitio en cuestión (como un evento o una fiesta por ejemplo).
+Contenido
+A través de la sección de “Discover” lanzada en enero del año pasado, Snapchat permite a los principales medios de comunicación mostrar contenido en formato de pequeños videos. No está claro cuánto genera Snapchat con este negocio, pero la distribución de contenido se ha convertido desde luego en un punto fuerte de su monetización.
+Comunicación
+Actualmente Snapchat no tiene una forma directa para que las empresas conecten directamente con los usuarios a través del chat, como sí Facebook algunas empresas en Facebook. Sin embargo Spiegel menciona de manera específica cómo la comunicación será una pieza del modelo empresarial de Snapchat dentro del plan de monetización.
+.</p>
+
+                        </div>
+
+
+                        <!--/.col-xs-6.col-lg-4-->
+                        <!--/.col-xs-6.col-lg-4-->
+                        <!--/.col-xs-6.col-lg-4-->
+                        <!--/.col-xs-6.col-lg-4-->
+                        <!--/.col-xs-6.col-lg-4-->
+                        <!--/.col-xs-6.col-lg-4-->
+                    </div>
+                    <!--/row-->
+                </div>
+                <!--/.col-xs-12.col-sm-9-->
+                <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+                    <div class="list-group">
+                        <a href="#" class="list-group-item active">Referencias</a>
+                        <a href="http://www.bbc.com/mundo/noticias/2016/01/160120_tecnologia_whatsapp_servicio_gratuito_il" class="list-group-item">Referencia 1</a>
+                        <a href="http://archivo.eluniversal.com.mx/articulos/67833.html" class="list-group-item">Referencia 2</a>
+                        <a href="http://www.elfinanciero.com.mx/tv/como-genera-dinero-snapchat.html" class="list-group-item">Referencia 3</a>
+                    </div>
+                </div>
+                <!--/.sidebar-offcanvas-->
+            </div>
+            <!--/row-->
+            <hr>
+            <footer>
+                <p>&copy; FISMAT 2017</p>
+            </footer>
+        </div>
+        <!--/.container-->
+        <!-- Bootstrap core JavaScript
+    ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+        <script>
+        $(document).ready(function () {
+            $('[data-toggle="offcanvas"]').click(function () {
+                $('.row-offcanvas').toggleClass('active')
+            });
+        });
+    </script>
+    </body>
+</html>
+Contact GitHub API Training Shop Blog About
+©
